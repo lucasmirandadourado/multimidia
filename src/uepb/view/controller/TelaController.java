e7548c1ba4662aed76f944430650b9a485cb58a5
fabinhodsouza <fabinho.dias.souza@gmail.com>
2015-05-07 22:15:53 -0300

    Interface bunitiiiinha!!
    tudo pronto em relação a interface

--- src/uepb/view/controller/TelaController.java
+++ src/uepb/view/controller/TelaController.java
@@ -8,16 +8,22 @@ import org.opencv.highgui.Highgui;
 
 import uepb.controller.ConverterBlocosParaYUV;
 import uepb.model.Bloco;
+import uepb.view.controller.abaParte1.AbaParte1;
+import uepb.view.controller.abaParte2.AbaParte2;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 
 public class TelaController implements Initializable {
+
+	// PARTE 1
 
 	@FXML
 	private ToggleButton toggleButtonCarregarCeu;
@@ -40,131 +46,58 @@ public class TelaController implements Initializable {
 	@FXML
 	private ImageView imageViewCarregarCeuEGrama;
 
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
+	// PARTE 2
 
-		toggleButtonCarregarGrama.setOnAction(new EventHandler<ActionEvent>() {
+	@FXML
+	private ToggleButton toggleButtonDCTCeu;
+	@FXML
+	private ToggleButton toggleButtonDCTGrama;
+	@FXML
+	private ToggleButton toggleButtonDCTCeuEGrama;
 
+	@FXML
+	private Pane paneMatriz;
+	@FXML
+	private Pane paneGrafico;
+
+	@FXML
+	private TextField textFieldZeros;
+
+	@FXML
+	private ImageView imageViewDCTCeu;
+	@FXML
+	private ImageView imageViewDCTGrama;
+	@FXML
+	private ImageView imageViewDCTCeuEGrama;
+
+	// Controllers
+
+	private AbaParte1 abaParte1;
+	private AbaParte2 abaParte2;
+
+	public void initialize(final URL location, final ResourceBundle resources) {
+
+		Platform.runLater(new Runnable() {
 			@Override
-			public void handle(ActionEvent event) {
-				
-				toggleButtonCarregarGrama.setSelected(true);
-				toggleButtonCarregarCeu.setSelected(false);
-				toggleButtonCarregarCeuEGrama.setSelected(false);
+			public void run() {
 
-				Image y = new Image("/uepb/icones/gramaY.jpg");
-				Image u = new Image("/uepb/icones/gramaU.jpg");
-				Image v = new Image("/uepb/icones/gramaV.jpg");
+				abaParte1 = new AbaParte1(toggleButtonCarregarCeu,
+						toggleButtonCarregarGrama,
+						toggleButtonCarregarCeuEGrama, paneY, paneU, paneV,
+						imageViewCarregarCeu, imageViewCarregarGrama,
+						imageViewCarregarCeuEGrama);
 
-				ImageView Iy = new ImageView();
-				ImageView Iu = new ImageView();
-				ImageView Iv = new ImageView();
+				abaParte2 = new AbaParte2(toggleButtonDCTCeu,
+						toggleButtonDCTGrama, toggleButtonDCTCeuEGrama,
+						paneMatriz, paneGrafico, textFieldZeros,
+						imageViewDCTCeu, imageViewDCTGrama,
+						imageViewDCTCeuEGrama);
 
-				Iy.setImage(y);
-				Iu.setImage(u);
-				Iv.setImage(v);
-
-				Iy.setFitHeight(220);
-				Iy.setFitWidth(220);
-
-				Iu.setFitHeight(110);
-				Iu.setFitWidth(110);
-
-				Iv.setFitHeight(110);
-				Iv.setFitWidth(110);
-				
-				//Iv.setOnZoom(null);
-
-				paneY.getChildren().add(Iy);
-				paneU.getChildren().add(Iu);
-				paneV.getChildren().add(Iv);
+				abaParte1.initialize(location, resources);
+				abaParte2.initialize(location, resources);
 			}
 		});
 
-		toggleButtonCarregarCeu.setOnAction(new EventHandler<ActionEvent>() {
-
-			@Override
-			public void handle(ActionEvent event) {
-				
-				toggleButtonCarregarGrama.setSelected(false);
-				toggleButtonCarregarCeu.setSelected(true);
-				toggleButtonCarregarCeuEGrama.setSelected(false);
-
-				Image y = new Image("/uepb/icones/ceuY.jpg");
-				Image u = new Image("/uepb/icones/ceuU.jpg");
-				Image v = new Image("/uepb/icones/ceuV.jpg");
-
-				ImageView Iy = new ImageView();
-				ImageView Iu = new ImageView();
-				ImageView Iv = new ImageView();
-
-				Iy.setImage(y);
-				Iu.setImage(u);
-				Iv.setImage(v);
-
-				Iy.setFitHeight(220);
-				Iy.setFitWidth(220);
-
-				Iu.setFitHeight(110);
-				Iu.setFitWidth(110);
-
-				Iv.setFitHeight(110);
-				Iv.setFitWidth(110);
-
-				paneY.getChildren().add(Iy);
-				paneU.getChildren().add(Iu);
-				paneV.getChildren().add(Iv);
-			}
-		});
-		
-		toggleButtonCarregarCeuEGrama.setOnAction(new EventHandler<ActionEvent>() {
-
-			@Override
-			public void handle(ActionEvent event) {
-				
-				toggleButtonCarregarGrama.setSelected(false);
-				toggleButtonCarregarCeu.setSelected(false);
-				toggleButtonCarregarCeuEGrama.setSelected(true);
-
-				Image y = new Image("/uepb/icones/ceuGramaY.jpg");
-				Image u = new Image("/uepb/icones/ceuGramaU.jpg");
-				Image v = new Image("/uepb/icones/ceuGramaV.jpg");
-
-				ImageView Iy = new ImageView();
-				ImageView Iu = new ImageView();
-				ImageView Iv = new ImageView();
-
-				Iy.setImage(y);
-				Iu.setImage(u);
-				Iv.setImage(v);
-
-				Iy.setFitHeight(220);
-				Iy.setFitWidth(220);
-
-				Iu.setFitHeight(110);
-				Iu.setFitWidth(110);
-
-				Iv.setFitHeight(110);
-				Iv.setFitWidth(110);
-
-				paneY.getChildren().add(Iy);
-				paneU.getChildren().add(Iu);
-				paneV.getChildren().add(Iv);
-			}
-		});
-
-		Image imagemCarregarGrama = new Image("/uepb/icones/grama.png");
-		imageViewCarregarGrama.setImage(imagemCarregarGrama);
-
-		Image imagemCarregarCeu = new Image("/uepb/icones/ceu.png");
-		imageViewCarregarCeu.setImage(imagemCarregarCeu);
-
-		Image imagemCarregarCeuEGrama = new Image("/uepb/icones/ceuGrama.png");
-		imageViewCarregarCeuEGrama.setImage(imagemCarregarCeuEGrama);
-
-		toggleButtonCarregarCeu.setGraphic(imageViewCarregarCeu);
-		toggleButtonCarregarGrama.setGraphic(imageViewCarregarGrama);
-		toggleButtonCarregarCeuEGrama.setGraphic(imageViewCarregarCeuEGrama);
 	}
 
 }
